b9f2aba10925c2921da2a752428c5ba25c2db8e9
popobg <pauline.bouyssou1@gmail.com>
2024-10-07 19:02:27 +0200

    Add Exercice Theatre

---
+++ src/main/java/fr/diginamic/entites/Theatre.java
@@ -0,0 +1,76 @@
+package fr.diginamic.entites;
+
+public class Theatre {
+    private String nom;
+    private int capaciteMax;
+    private int nbClientsInscrits;
+    private double recetteTotale;
+
+    public Theatre(String nom, int nbClientsInscrits, int capaciteMax, double recetteTotale) {
+        this.recetteTotale = recetteTotale;
+        this.nom = nom;
+        this.nbClientsInscrits = nbClientsInscrits;
+        this.capaciteMax = capaciteMax;
+    }
+
+    /**
+     * Ajoute les clients donnés aux clients inscrits
+     * tant que le théâtre n'est pas rempli, c'est-à-dire tant que
+     * la capacité max est supérieure au nombre de clients inscrits
+     * @param nbClientsAInscrire nombre de clients qui souhaitent s'inscrire
+     * @param prixPlace prix de la place pour cette représentation
+     * @return "true" si tous les clients donnés ont pu être inscrits,
+     *          "false" si tous les clients n'ont pas pu être inscrits
+     *          (plus de place)
+     */
+    public boolean InscrireClients(int nbClientsAInscrire, double prixPlace) {
+        while (this.capaciteMax > nbClientsInscrits && nbClientsAInscrire > 0) {
+            this.nbClientsInscrits += 1;
+            nbClientsAInscrire -= 1;
+
+            // On encaisse le client
+            this.setRecetteTotale(this.recetteTotale + prixPlace);
+        }
+
+        // le message de capacité max peut s'afficher même si tous les spectateurs
+        // donnés en paramètre ont été placés, c'est-à-dire s'il y a pile
+        // le bon nombre de places
+        if (this.capaciteMax == nbClientsInscrits) {
+            this.afficherMessageErreur("Le théâtre n'accueille plus de spectateur.");
+        }
+        
+        return nbClientsAInscrire > 0 ? false : true;
+    }
+
+    private void afficherMessageErreur(String message) {
+        System.out.println(message);
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nouveauNom) {
+        this.nom = nouveauNom;
+    }
+
+    public int getCapaciteMax() {
+        return capaciteMax;
+    }
+
+    public void setCapaciteMax(int nouvelleCapacite) {
+        this.capaciteMax = nouvelleCapacite;
+    }
+
+    public int getNbClientsInscrits() {
+        return nbClientsInscrits;
+    }
+
+    public double getRecetteTotale() {
+        return recetteTotale;
+    }
+
+    public void setRecetteTotale(double nouvelleRecette) {
+        this.recetteTotale = nouvelleRecette;
+    }
+}
---
+++ src/main/java/fr/diginamic/essais/TestTheatre.java
@@ -0,0 +1,24 @@
+package fr.diginamic.essais;
+
+import fr.diginamic.entites.Theatre;
+
+public class TestTheatre {
+    public static void main(String[] args) {
+        Theatre theatre = new Theatre("Théâtre du Grand Rond", 3, 50, 25.5);
+
+        System.out.println("Nombre de clients inscrits : " + theatre.getNbClientsInscrits());
+        System.out.println();
+
+        // print true si on a pu inscrire tous les clients donnés
+        // en paramètres à la méthode
+        System.out.println(theatre.InscrireClients(50, 8.5));
+        System.out.println(theatre.InscrireClients(10, 8.5));
+        System.out.println(theatre.InscrireClients(10, 8.5));
+        System.out.println(theatre.InscrireClients(10, 8.5));
+        System.out.println(theatre.InscrireClients(10, 8.5));
+
+        System.out.println();
+        System.out.println("Nombre de clients inscrits : " + theatre.getNbClientsInscrits());
+        System.out.println("Recette totale: " + theatre.getRecetteTotale());
+    }
+}
