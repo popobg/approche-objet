8a6fb55145a6b6a07a5a90d8648a1aa3c7da26a2
popobg <pauline.bouyssou1@gmail.com>
2024-10-14 14:39:16 +0200

    Variables renaming

--- src/main/java/fr/diginamic/dates/TestCalendar.java
+++ src/main/java/fr/diginamic/dates/TestCalendar.java
@@ -9,25 +9,28 @@ public class TestCalendar {
     public static void main(String[] args) {
         Calendar cal1 = Calendar.getInstance();
         cal1.set(2016, 04, 19, 23, 59, 30);
-        Date date = cal1.getTime();
+        Date date2016 = cal1.getTime();
 
-        SimpleDateFormat formateurCourt = new SimpleDateFormat("dd'/'MM'/'yyyy");
+        SimpleDateFormat formateurCourt = new SimpleDateFormat("dd/MM/yyyy");
 
-        System.out.println(formateurCourt.format(date));
+        System.out.println(formateurCourt.format(date2016));
 
         Calendar cal2 = Calendar.getInstance();
         Date dateJour = cal2.getTime();
 
-        SimpleDateFormat formateurLong = new SimpleDateFormat("EEEE dd MMMM yyyy HH':'mm':'ss");
-        SimpleDateFormat formateurLongFr = new SimpleDateFormat("EEEE dd MMMM yyyy HH':'mm':'ss", Locale.FRANCE);
-        SimpleDateFormat formateurLongChinois = new SimpleDateFormat("EEEE dd MMMM yyyy HH':'mm':'ss", Locale.CHINA);
-        SimpleDateFormat formateurLongRusse = new SimpleDateFormat("EEEE dd MMMM yyyy HH':'mm':'ss", new Locale("ru"));
-        SimpleDateFormat formateurLongAllemand = new SimpleDateFormat("EEEE dd MMMM yyyy HH':'mm':'ss", Locale.GERMAN);
+        String pattern = "EEEE dd MMMM yyyy HH:mm:ss";
+
+        SimpleDateFormat formateurLong = new SimpleDateFormat(pattern);
+        SimpleDateFormat formateurLongFr = new SimpleDateFormat(pattern, Locale.FRANCE);
+        SimpleDateFormat formateurLongCh = new SimpleDateFormat(pattern, Locale.CHINA);
+        // Remplace "new Locale(String)"
+        SimpleDateFormat formateurLongRu = new SimpleDateFormat(pattern, Locale.of("ru", "RU"));
+        SimpleDateFormat formateurLongGe = new SimpleDateFormat(pattern, Locale.GERMAN);
 
         System.out.println(formateurLong.format(dateJour));
         System.out.println(formateurLongFr.format(dateJour));
-        System.out.println(formateurLongChinois.format(dateJour));
-        System.out.println(formateurLongRusse.format(dateJour));
-        System.out.println(formateurLongAllemand.format(dateJour));
+        System.out.println(formateurLongCh.format(dateJour));
+        System.out.println(formateurLongRu.format(dateJour));
+        System.out.println(formateurLongGe.format(dateJour));
     }
 }
--- src/main/java/fr/diginamic/dates/TestDates.java
+++ src/main/java/fr/diginamic/dates/TestDates.java
@@ -7,19 +7,17 @@ public class TestDates {
     public static void main(String[] args) {
         Date dateJour1 = new Date();
 
-        SimpleDateFormat formateurDateJour = new SimpleDateFormat("dd'/'MM'/'yyyy");
+        SimpleDateFormat formateurDateJour = new SimpleDateFormat("dd/MM/yyyy");
 
         System.out.println(formateurDateJour.format(dateJour1));
 
         // 19/05/2016, 23:59:30
         Date dateDeprecated = new Date(116, 04, 19, 23, 59, 30);
 
-        SimpleDateFormat formateurDateDeprecated = new SimpleDateFormat("yyyy'/'MM'/'dd HH':'mm':'ss");
+        SimpleDateFormat formateurDateDeprecated = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 
         System.out.println(formateurDateDeprecated.format(dateDeprecated));
 
-        Date dateJour2 = new Date();
-
-        System.out.println(formateurDateDeprecated.format(dateJour2));
+        System.out.println(formateurDateDeprecated.format(new Date()));
     }
 }
